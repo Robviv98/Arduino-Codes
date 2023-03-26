274999b74d9c1659c4cb762453cda84359c86d37
Robviv98 <robertovives27@gmail.com>
2023-03-26 19:41:46 -0300

    subida de archivos

--- Scripts/puertaLaser/puertaLaser.ino
+++ Scripts/puertaLaser/puertaLaser.ino
@@ -1,15 +1,16 @@
 int uno = 2;
 int dos = 3;
 
+int luz=4;
+
 int personas = 0;
-int espera = 1000;
+
 
 bool entrada = false;
 bool salida = false;
-bool siglo=false;
 
 void deteccion() {
-  if (entrada == false and salida == false and siglo == false) {
+  if (entrada == false and salida == false) {
     if (digitalRead(uno) == LOW and digitalRead(dos) == HIGH) {
       entrada = true;
     }
@@ -23,7 +24,9 @@ void deteccion() {
       personas++;
       entrada = false;
       Serial.println("Personas en la habitacion: " + String(personas));
-      delay(espera);
+      while(digitalRead(dos) == LOW){
+
+      }
     }
   }
   if (salida) {
@@ -31,8 +34,28 @@ void deteccion() {
       personas--;
       salida = false;
       Serial.println("Personas en la habitacion: " + String(personas));
-      delay(espera);
+      while(digitalRead(uno) == LOW){
+        
+      }
     }
+  }
+}
+
+void proteccion(){
+  if(personas<0){
+    personas=0;
+    Serial.println("error Conteo de personas negativo, se restablecera a 0");
+  }
+}
+
+void luces(){
+  if(personas>0){
+    digitalWrite(luz,HIGH);
+    
+  }
+  else{
+    digitalWrite(luz,LOW);
+
   }
 }
 
@@ -40,9 +63,12 @@ void setup() {
   Serial.begin(9600);
   pinMode(uno, INPUT);
   pinMode(dos, INPUT);
+  pinMode(luz, OUTPUT);
 }
 
 void loop() {
+  luces();
+  proteccion();
   deteccion();
   
 
